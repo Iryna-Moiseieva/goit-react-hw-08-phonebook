3b6a1a8651a06f23d493c0f3855098c21d64b673
Iryna Moiseieva <i.moiseieva@godeltech.com>
2022-10-09 14:13:40 +0200

    fix styles

--- src/pages/HomePage/HomePage.styles.js
+++ src/pages/HomePage/HomePage.styles.js
@@ -4,11 +4,11 @@ export const Page = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  min-height: calc(100vh - 50px);
 `;
 
-export const Title = styled.div`
-font-weight: 700;
+export const Title = styled.h1`
+  margin: 0 auto;
+  font-weight: 700;
   color: var(--color-gray-300);
-  font-size: var(--font-size-800);
+  font-size: 60px;
 `;
